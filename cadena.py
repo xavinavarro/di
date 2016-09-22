e5e4706fca64ae949097b99191cab88bdedb61b5
XaviNavarro <dam.xnavarro@institutoserpis.org>
2016-09-22 12:55:42 +0200

    Hola y Cadena

---
+++ cadena.py
@@ -0,0 +1,9 @@
+#Cadena
+c = "Hola Mundo"
+
+#Esto un entero 
+e = 23
+
+#Se comprueba asi
+type(c)
+type(e)
---
+++ hola.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+print 'Hola Mundo'
+a = raw_input()
+print a
+
+#Mi primer programa en python
+#Cadena
+c = 'Hola Mundo'
+
+#Esto un entero 
+e = 23
+
+#Se comprueba asi
+print type(c)
+print type(e)
+
