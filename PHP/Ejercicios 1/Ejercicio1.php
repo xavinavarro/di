590722cf325c21a221c9c0134aff2964378d0973
xavinavarro <dam.xnavarro@institutoserpis.org>
2017-01-19 12:23:26 +0100

    Ejercicios PHP

---
+++ PHP/Ejercicios 1/Ejercicio1.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>ejercicio 1</title>
+	</head>
+	<body>
+		<?php
+			$ini = "Hola ";
+			$fin = " hola piter amigo mio";
+			$todo = $ini.$fin;
+			echo $todo;
+		?>
+	</body>
+</html>
