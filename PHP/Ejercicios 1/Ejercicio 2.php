37b13a1faec25a43f3fc3ff33f35339ffae81681
xavinavarro <dam.xnavarro@institutoserpis.org>
2017-01-19 12:34:20 +0100

    Update

---
+++ PHP/Ejercicios 1/Ejercicio 2.php
@@ -0,0 +1,7 @@
+<?php
+	$n1=1;
+	$n2=2;
+	$suma=$n1+$n2;
+	echo "$n1+$n2 <br>";
+	echo "suman = ".$suma;
+?>
--- PHP/Ejercicios 1/Ejercicio1.php
+++ PHP/Ejercicios 1/Ejercicio1.php
@@ -1,13 +1,7 @@
-<html>
-	<head>
-		<title>ejercicio 1</title>
-	</head>
-	<body>
-		<?php
-			$ini = "Hola ";
-			$fin = " hola piter amigo mio";
-			$todo = $ini.$fin;
-			echo $todo;
-		?>
-	</body>
-</html>
+<?php
+	$ini = "Hola ";
+	$fin = " hola piter amigo mio";
+	$todo = $ini.$fin;
+	echo $todo;
+?>
+
